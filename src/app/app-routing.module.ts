ca99d271191b37a13f8f42f30c68113d360cbea4
Margalit-Abermad <2329767+0@gmail.com>
2022-10-27 02:02:29 +0300

    2 login components(login+loginManager)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginManagerComponent } from './login-manager/login-manager.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // {path:'',component:AppComponent},
+  {path:'LoginManager',component:LoginManagerComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AuthServiceService } from './Services/auth-service.service';
+import { LoginManagerComponent } from './login-manager/login-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    LoginManagerComponent
+ ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
